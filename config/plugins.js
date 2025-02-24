e5d089f7cf5418f52d2d2b1cac77c7617b090a05
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-02-24 14:34:43 +0300

    s

--- config/plugins.js
+++ config/plugins.js
@@ -35,17 +35,35 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "strapi-provider-cloudflare-r2",
       providerOptions: {
-        credentials: {
-          accessKeyId: env("R2_ACCESS_KEY_ID"),
-          secretAccessKey: env("R2_ACCESS_SECRET"),
-        },
-        region: env("R2_REGION"),
+        accessKeyId: env("R2_ACCESS_KEY_ID"),
+        secretAccessKey: env("R2_ACCESS_SECRET"),
+        /**
+         * `https://<ACCOUNT_ID>.r2.cloudflarestorage.com`
+         */
         endpoint: env("R2_ENDPOINT"),
         params: {
           Bucket: env("R2_BUCKET"),
         },
+        /**
+         * Set this Option to store the CDN URL of your files and not the R2 endpoint URL in your DB.
+         * Can be used in Cloudflare R2 with Domain-Access or Public URL: https://pub-<YOUR_PULIC_BUCKET_ID>.r2.dev
+         * This option is required to upload files larger than 5MB, and is highly recommended to be set.
+         * Check the cloudflare docs for the setup: https://developers.cloudflare.com/r2/data-access/public-buckets/#enable-public-access-for-your-bucket
+         */
+        cloudflarePublicAccessUrl: env("R2_PUBLIC_ACCESS_URL"),
+        /**
+         * Sets if all assets should be uploaded in the root dir regardless the strapi folder.
+         * It is useful because strapi sets folder names with numbers, not by user's input folder name
+         * By default it is false
+         */
+        pool: false,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
